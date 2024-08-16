b275e06724f446eaec5d7b41eabca7c0f753ca5b
Andrew Hagner <andrewx991@gmail.com>
2024-08-16 01:29:44 +0000

    Eliminate extra TITHE variable.

--- program.cpp
+++ program.cpp
@@ -7,10 +7,10 @@ int main()
 	const double RATE_2 = .10;
 	const double RATE_3 = .20;
 
-	int TITHE_2 = 0;
-	int TITHE_3 = 0;
+	int recommended_tithe = 0;
 
 	double income;
+
 	cout << "Please enter your income:" << endl;
 	cin >> income;
 	cout << "Thank you!" << endl;
@@ -21,14 +21,14 @@ int main()
 	}
 	else if (income <= 80000)
 	{
-		TITHE_2 = RATE_2 * income;
-		cout << "Your total suggested tithe for the year is" << " " << TITHE_2 << endl;
+		recommended_tithe = RATE_2 * income;
+		cout << "Your total suggested tithe for the year is" << " " << recommended_tithe << endl;
 		cout << "Thank you for your donation!" << endl;
 	}
 	else
 	{
-		TITHE_3 = RATE_3 * income;
-		cout << "Your total suggested tithe for the year is" << " " << TITHE_3 << endl;
+		recommended_tithe = RATE_3 * income;
+		cout << "Your total suggested tithe for the year is" << " " << recommended_tithe << endl;
 		cout << "Thank you for your donation!" << endl;
 	}
 
