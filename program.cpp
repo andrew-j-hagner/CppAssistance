d15b8aad04bb21dd67ada9c7ce7b425a537bf8d1
Andrew Hagner <andrewx991@gmail.com>
2024-08-16 01:08:04 +0000

    Implemented consistent output formatting.

--- program.cpp
+++ program.cpp
@@ -16,18 +16,17 @@ int main()
 	double income;
 	cout << "Please enter your income:" << endl;
 	cin >> income;
-
-	cout << "Thank you!";
+	cout << "Thank you!" << endl;
 
 	if (income <= 45000)
 	{
-		cout << "No tithes suggested, thank you for your support and please pray for us!";
+		cout << "No tithes suggested, thank you for your support and please pray for us!" << endl;
 	}
 	else (income >= 80000);
 
 		TITHE_2 = RATE_2 * income;
 		cout << "Your total suggested tithe for the year is" << " " << TITHE_2 << endl;
-		cout << "Thank you for your donation!";
+		cout << "Thank you for your donation!" << endl;
 
 		return 0;
 }
