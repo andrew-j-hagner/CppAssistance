caac49d5f117fc1163e502264f92ed34bd3bed72
Andrew Hagner <andrewx991@gmail.com>
2024-08-16 01:22:43 +0000

    Implemented correct behavior as explained in email.

--- program.cpp
+++ program.cpp
@@ -10,23 +10,32 @@ int main()
 
 	int TITHE_1;
 	int TITHE_2;
+	int TITHE_3;
 	TITHE_1 = 0;
 	TITHE_2 = 0;
+	TITHE_3 = 0;
 
 	double income;
 	cout << "Please enter your income:" << endl;
 	cin >> income;
 	cout << "Thank you!" << endl;
 
-	if (income <= 45000)
+	if (income <= 40000)
 	{
 		cout << "No tithes suggested, thank you for your support and please pray for us!" << endl;
 	}
-	else (income >= 80000);
-
+	else if (income <= 80000)
+	{
 		TITHE_2 = RATE_2 * income;
 		cout << "Your total suggested tithe for the year is" << " " << TITHE_2 << endl;
 		cout << "Thank you for your donation!" << endl;
+	}
+	else
+	{
+		TITHE_3 = RATE_3 * income;
+		cout << "Your total suggested tithe for the year is" << " " << TITHE_3 << endl;
+		cout << "Thank you for your donation!" << endl;
+	}
 
-		return 0;
+	return 0;
 }
